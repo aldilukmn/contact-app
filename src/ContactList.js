a730b882947ca306b92724e413ab8dd53b86caea
Aldi Lukman Maulana Latief <aldilukman99@gmail.com>
2023-04-10 22:59:44 +0700

    react contact app

---
+++ src/ContactList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ContactItem from "./ContactItem";
+
+function ContactList({ contacts }) {
+  return (
+    <div className="contact_list">
+      {contacts.map((contact) => (
+        <ContactItem key={contact.id} {...contact} />
+      ))}
+    </div>
+  );
+}
+
+export default ContactList;
