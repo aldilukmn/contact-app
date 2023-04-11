6e97036bd67e70c75c15d2bd269a0923318d418e
Aldi Lukman Maulana Latief <aldilukman99@gmail.com>
2023-04-11 12:58:46 +0700

    module styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ContactApp from "./ContactApp";
+import ContactApp from "./components/ContactApp";
+import "./style/style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<ContactApp />);
