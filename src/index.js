ffbfa8e2c6173cdba6aab655863c70423ae2ce9f
Aldi Lukman Maulana Latief <aldilukman99@gmail.com>
2023-04-11 12:54:58 +0700

    static styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import ContactApp from "./ContactApp";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<ContactApp/>)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<ContactApp />);
