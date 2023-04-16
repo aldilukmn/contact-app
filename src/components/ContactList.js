605d175ff7a457c56d9b95233c197bf9cc4a5544
Aldi Lukman Maulana Latief <aldilukman99@gmail.com>
2023-04-16 17:40:55 +0700

    update no contact logic

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ContactItem from "./ContactItem";
+import NoContact from "./NoContact";
 
 function ContactList({ contacts, onDelete }) {
   return (
     <div className="contact_list">
-      {contacts.map((contact) => (
+      {contacts.length === 0 ? (
+        <NoContact/>
+      ) : (
+      contacts.map((contact) => (
         <ContactItem key={contact.id} id={contact.id} onDelete={onDelete} {...contact} />
-      ))}
+      ))
+      )}
     </div>
   );
 }
---
+++ src/components/NoContact.js
@@ -0,0 +1,9 @@
+function NoContact() {
+  return (
+    <div class="no_contact">
+      <h1>Tidak ada kontak</h1>
+    </div>
+  );
+}
+
+export default NoContact;
