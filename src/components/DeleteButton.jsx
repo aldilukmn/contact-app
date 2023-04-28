4aa9c87ea29c3e9e9c7b5212745bb7c4fb227756
Aldi Lukman Maulana Latief <aldilukman99@gmail.com>
2023-04-28 21:18:13 +0700

    implement : search component, search parameter, react icons

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,9 +1,10 @@
 import PropTypes from "prop-types";
+import { FiDelete } from "react-icons/fi";
 
 function DeleteButton({ id, onDelete }) {
   return (
     <button className="contact_item_delete" onClick={() => onDelete(id)}>
-      X
+      <FiDelete/>
     </button>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 import ContactList from "../components/ContactList";
+import SearchBar from "../components/SearchBar";
 import { getContacts, deleteContact } from "../utils/data";
+import { useSearchParams } from "react-router-dom";
+
+function HomePageWrapper() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const keyword = searchParams.get("keyword");
+
+  function changeSearchParams(keyword) {
+    setSearchParams({ keyword });
+  }
+
+  return <HomePage defaultKeyword={keyword} keywordChange={changeSearchParams} />;
+}
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -8,9 +22,11 @@ class HomePage extends React.Component {
 
     this.state = {
       contacts: getContacts(),
+      keyword: props.defaultKeyword || "",
     };
 
     this.onDeleteHandler = this.onDeleteHandler.bind(this);
+    this.onKeyWordChangeHandler = this.onKeyWordChangeHandler.bind(this);
   }
 
   onDeleteHandler(id) {
@@ -23,14 +39,28 @@ class HomePage extends React.Component {
     });
   }
 
+  onKeyWordChangeHandler(keyword) {
+    this.setState(() => {
+      return {
+        keyword,
+      };
+    });
+    this.props.keywordChange(keyword);
+  }
+
   render() {
+    const contacts = this.state.contacts.filter((contact) => {
+      return contact.name.toLowerCase().includes(this.state.keyword.toLowerCase());
+    });
+
     return (
       <section>
+        <SearchBar keyword={this.state.keyword} keywordChange={this.onKeyWordChangeHandler} />
         <h2>Daftar Kontak</h2>
-        <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler}></ContactList>
+        <ContactList contacts={contacts} onDelete={this.onDeleteHandler}></ContactList>
       </section>
     );
   }
 }
 
-export default HomePage;
+export default HomePageWrapper;
